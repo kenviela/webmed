edb4dbc1b277e8f716089ff426429ab610009be3
daniela guzman <danielaguzman50@outlook.es>
2023-05-25 15:35:17 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/finances" element={<Finances />} />
